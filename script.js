6ddb0e70da0340b895c1e1f32f3e3fc1256f5f38
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-26 18:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let line = document.getElementById("line");
+let count = 180;
+ document.addEventListener("DOMContentLoaded" , () =>{
+	 setInterval(()=>{
+	 line.style.transform = `rotate(${count}deg)`;
+		 count=count+2;
+		 if (count >= 360) {
+            count = 0; 
+        }
+	 },20)
+ })
